ea40145ed5600ecd1199f09a49df470c81f78cc6
Anderson Santos <anderson.santos@eunerd.com.br>
2022-04-17 01:46:22 -0300

    Conclusão do Modulo

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,11 +3,15 @@ import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss';
 import { useState, useEffect } from "react";
 
-// https://api.github.com/users/anderood/repos
+interface Repository {
+    name: string;
+    description: string;
+    html_url: string;
+}
 
 export function RepositoryList() {
 
-    const [repositories, setRepositories] = useState([]);
+    const [repositories, setRepositories] = useState<Repository[]>([]);
 
     useEffect(()=>{
         fetch('https://api.github.com/users/anderood/repos')
